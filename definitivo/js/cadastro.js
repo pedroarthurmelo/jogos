fae68b99d2a00659d55c474eef7c943da40f67b7
Pedro Arthur Melo Rodrigues <pedro.arthur1@pucpr.edu.br>
2025-03-26 22:30:51 -0300

    logica regex fazer

--- definitivo/js/cadastro.js
+++ definitivo/js/cadastro.js
@@ -2,5 +2,10 @@ function registrar(){
     let senha = document.getElementById("password").value;
     let hash = CryptoJS.SHA256(senha).toString();
     window.alert("Cadastro Realizado com Sucesso");
-    console.log("HASH TEST", hash);
+    console.log("Hash Gerado", hash);
+
+
+    if (senha < 8){
+        window.alert("A senha deve ser maior que 8 caracteres!")
+    }
 }
